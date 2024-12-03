6844e91d16674de2a3027d09284c55a3e87fdac3
nirmalkumar <nirmal.kumar@probeinformation.com>
2024-12-03 09:59:11 +0530

    added reading-time

--- reading-time/scripts/content.js
+++ reading-time/scripts/content.js
@@ -1,14 +1,15 @@
-const article = document.querySelector("article");
-const text = article.textContent
-const regExp = /\S+/g;
-const words = [...text.matchAll(regExp)];
-const readingTime = Math.round(words.length/200);
-console.log(readingTime)
-const badge = document.createElement("p");
-badge.classList.add("color-secondary-text", "type--caption");
-badge.textContent = `⏱️ ${readingTime} min read`;
-const heading = article.querySelector("h1");
-console.log(heading)
-// Support for article docs with date
-const date = article.querySelector("time")?.parentNode;
-(date ?? heading).insertAdjacentElement("afterend", badge);
+// const article = document.querySelector("article");
+// const text = article.textContent
+// const regExp = /\S+/g;
+// const words = [...text.matchAll(regExp)];
+// const readingTime = Math.round(words.length/200);
+// console.log(readingTime)
+// const badge = document.createElement("p");
+// badge.classList.add("color-secondary-text", "type--caption");
+// badge.textContent = `⏱️ ${readingTime} min read`;
+// const heading = article.querySelector("h1");
+// console.log(heading)
+// // Support for article docs with date
+// const date = article.querySelector("time")?.parentNode;
+// (date ?? heading).insertAdjacentElement("afterend", badge);
+console.log("reading time")
